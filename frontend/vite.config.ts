10b43e97683ec389bb055035549279c6c7c53dd4
amir.yousefi <amir.yousefi.info@gmail.com>
2024-02-04 22:56:16 +0100

    wip

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,38 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Cellular-Automata",
+        short_name: "cellular",
+        description: "Wolframs algorithm by Groupeffect",
+        background_color: "#000000",
+        display:"standalone",
+        icons: [
+            {
+              sizes:"144x144",
+              src:"assets/logo144.png",
+              type: "img/png"
+            },
+            {
+              sizes:"128x128",
+              src:"assets/logo128.png",
+              type: "img/png"
+            },
+            {
+              sizes:"48x48",
+              src:"assets/logo48.png",
+              type: "img/png"
+            },
+            {
+              sizes:"16x16",
+              src:"assets/logo16.png",
+              type: "img/png"
+            },
+          ]
+        }
+    })
   ],
   resolve: {
     alias: {
